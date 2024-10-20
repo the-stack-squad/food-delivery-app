c2bf1387bbbff0e82d97618f54e987cd9c01ee88
AstronautDesh <powerfistmarley@gmail.com>
2024-10-20 18:13:42 +0100

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     Dev Dynasty....
+      </header>
+    </div>
+  );
+}
+
+export default App;
